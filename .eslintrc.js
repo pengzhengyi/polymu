fe4c8905761e88922298acd6e1ff1452e6a88b1e
pengzhengyi <pengzhengyipengzhengyi@gmail.com>
2020-09-16 12:10:53 +0800

    update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,9 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['./tsconfig.json'],
-    tsconfigRootDir: __dirname,
-    warnOnUnsupportedTypeScriptVersion: false,
-  },
-  plugins: ['@typescript-eslint', 'jest'],
+  plugins: ['jest', '@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:jest/recommended',
@@ -17,11 +11,27 @@ module.exports = {
     'prettier/@typescript-eslint',
   ],
   env: {
+    'commonjs': true,
     'es6': true,
     'jest/globals': true,
   },
   rules: {
-    semi: ['error', 'always'],
-    quotes: ['error', 'double'],
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
+      {
+        'ts-expect-error': 'allow-with-description',
+        'ts-ignore': true,
+        'ts-nocheck': true,
+        'ts-check': false,
+        'minimumDescriptionLength': 5,
+      },
+    ],
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
