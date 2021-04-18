6f8c4cf0e7a2f2a54c637c1e8ba17732d703fe95
pengzhengyi <pengzhengyipengzhengyi@gmail.com>
2021-04-18 19:52:50 +0800

    add a ViewElementProvider that can convert DOM constructs to corresponding ViewElement hierarchy

---
+++ src/views/ViewElementProvider.test.ts
@@ -0,0 +1,70 @@
+import { ViewElement } from './ViewElement';
+import { ViewElementProvider } from './ViewElementProvider';
+
+function createListElement(numChild: number): HTMLOListElement {
+  const listElement = document.createElement('ol');
+  for (let i = 0; i < numChild; i++) {
+    const listItemElement = document.createElement('li');
+    listItemElement.textContent = i.toString();
+    listElement.appendChild(listItemElement);
+  }
+  return listElement;
+}
+
+describe('ViewElementProvider', () => {
+  test('initialize from HTMLTemplateElement', () => {
+    const templateElement = document.createElement('template');
+    const listElement = createListElement(10);
+    listElement.id = 'container-list';
+    templateElement.content.appendChild(listElement);
+
+    const viewElementProvider: ViewElementProvider = new ViewElementProvider();
+    viewElementProvider.consume(templateElement);
+
+    expect(viewElementProvider.parentViewElement.element_.id).toEqual('container-list');
+    expect(viewElementProvider.childViewElements).toHaveLength(10);
+  });
+
+  test('use fallback container when initialize from HTMLCollection', () => {
+    const listElement = createListElement(10);
+    listElement.id = 'container-list';
+
+    const fallbackContainer = document.createElement('ul');
+    fallbackContainer.id = 'fallback-container-list';
+
+    const viewElementProvider: ViewElementProvider = new ViewElementProvider();
+    viewElementProvider.consume(listElement.children, fallbackContainer);
+
+    expect(viewElementProvider.parentViewElement.element_.id).toEqual('fallback-container-list');
+    expect(viewElementProvider.parentViewElement.element_.tagName).toEqual('UL');
+    expect(viewElementProvider.childViewElements).toHaveLength(10);
+
+    const firstElement = (viewElementProvider.childViewElements as Array<ViewElement>)[0].element_;
+    expect(firstElement.parentElement).toBe(listElement);
+  });
+
+  test('lazy initialization from HTMLElement with large number of children', () => {
+    const numChild = 10;
+    const listElement = createListElement(numChild);
+    listElement.id = 'container-list';
+
+    const viewElementProvider: ViewElementProvider = new ViewElementProvider();
+    viewElementProvider.consume(listElement, undefined, true);
+    expect(viewElementProvider.parentViewElement.element_.id).toEqual('container-list');
+
+    // before materialization, no ViewElement has been added as child
+    expect(viewElementProvider.parentViewElement.children_).toHaveLength(0);
+
+    let i = 0;
+
+    // view element is added alongside first iteration
+    for (const childViewElement of viewElementProvider.childViewElements) {
+      expect(childViewElement.element_.textContent).toEqual(i.toString());
+      i++;
+      expect(viewElementProvider.parentViewElement.children_).toHaveLength(i);
+    }
+
+    // view element is all added after first iteration
+    expect(viewElementProvider.childViewElements).toHaveLength(numChild);
+  });
+});
---
+++ src/views/ViewElementProvider.ts
@@ -0,0 +1,179 @@
+/**
+ * @module
+ *
+ * This module provides interface `IViewElementProvider` and some utility classes implementing this interface.
+ */
+
+import { LazyCollectionProvider } from '../collections/Collection';
+import { ViewElement } from './ViewElement';
+
+/**
+ * This interface abstracts out a provider that returns parent `ViewElement` and child `ViewElement` on demand. This parent-child relationship can also be viewed as a container-item relationship -- all child `ViewElement` are contained/registered in parent `ViewElement`.
+ */
+export interface IViewElementProvider {
+  /**
+   * If exists, `parentViewElement` should provide a `ViewElement` that is parent for all `ViewElement` in `childViewElements`.
+   */
+  parentViewElement?: ViewElement;
+  /**
+   * An iterable of `ViewElement`.
+   *
+   * This iterable should allow multiple times of iteration. If these `ViewElement` can fit inside memory, then an Array or a `LazyCollectionProvider` will be the best choice. If the number of `ViewElement` is too large to fit inside memory, a custom built subclass of `UnmaterializableCollectionProvider` might be a better alternative.
+   */
+  childViewElements: Iterable<ViewElement>;
+}
+
+/**
+ * Extend `LazyCollectionProvider` to provide these additional functionalities:
+ *
+ * + caller can check whether materialization finished through `materialized`.
+ * + caller can execute custom logic before materialization through `materializationCallback`.
+ */
+class LazyCollectionProviderWithMaterializationCallback<TElement> extends LazyCollectionProvider<
+  TElement
+> {
+  /**
+   * A callback that when registered will be called before materialization happens. This can be used to define additional actions to execute during materialization.
+   *
+   * @callback
+   * @param index - The index of element to be materialized.
+   * @param materializingElement - The element about to be materialized.
+   */
+  materializationCallback: (index: number, materializingElement: TElement) => void;
+
+  /**
+   * Expose `this._materialized`.
+   *
+   * @alias _materialized
+   * @returns Whether the underlying iterable has been fully iterated and materialized.
+   */
+  get materialized(): boolean {
+    return this._materialized;
+  }
+
+  /**
+   * @override Invoke `materializationCallback` if bound before materialize an element.
+   */
+  protected materializeElement(index: number, element: TElement) {
+    this.materializationCallback && this.materializationCallback(index, element);
+    this._materializedCollection[index] = element;
+  }
+}
+
+/**
+ * A union type that contains all types which can be used to initialize `ViewElementProvider`.
+ *
+ * Since HTMLTemplateElement (`<template>`)'s content is a DocumentFragment and since DocumentFragment implements the ParentNode interface, source can therefore be classified into one of two forms:
+ *
+ *    + (rooted) a node who will be the parent view element and whose children will be the child view elements
+ *    + (unrooted) a collection of elements which will themselves be child view elements
+ */
+type TSourceType =
+  | HTMLTemplateElement
+  | DocumentFragment
+  | HTMLCollection
+  | HTMLElement
+  | Iterable<HTMLElement>;
+
+/**
+ * This class provides the ability to parse DOM constructs like `HTMLCollection`, `HTMLElement`, `HTMLTemplateElement` to provide parent and children `ViewElement`.
+ *
+ * After creating an instance of `ViewElementProvider`, an invocation of `consume` method is necessary to start parsing DOM constructs and building `ViewElement` hierarchy.
+ */
+export class ViewElementProvider implements IViewElementProvider {
+  /**
+   * A threshold above which lazy initialization will be applied. When lazy initialization is applied, children `ViewElement` will not be added to parent `ViewElement` immediately. Rather it will be added before this `ViewElement` is about to be iterated over.
+   */
+  static LAZY_INITIALIZATION_THRESHOLD: number = 10000;
+
+  /** @override */
+  parentViewElement: ViewElement;
+
+  /**
+   * Internal implementation to provide children `ViewElement`.
+   */
+  protected getChildViewElementsInternal: () => Iterable<ViewElement>;
+
+  /** @override */
+  get childViewElements(): Iterable<ViewElement> {
+    return this.getChildViewElementsInternal();
+  }
+
+  /**
+   * Consume DOM construct so that current `ViewElementProvider` is able to provide corresponding `ViewElement` construct.
+   *
+   * @param source - A DOM construct provides DOM element(s) to bootstrap corresponding `ViewElement` hierarchy.
+   * @param {HTMLElement} [fallbackContainer] - An DOM element which will be used to create parent `ViewElement` if `source` is "unrooted". An error will be raised if this argument is not provided when `source` is "unrooted".
+   * @param {boolean} [shouldLazyInitialize = undefined] - Whether children `ViewElement` will be lazily registered to the parent `ViewElement`. Default to `undefined`, which means that lazy initialization is enabled when the number of child DOM elements exceeds the threshold.
+   */
+  consume(
+    source: TSourceType,
+    fallbackContainer?: HTMLElement,
+    shouldLazyInitialize: boolean = undefined
+  ) {
+    if (source instanceof HTMLTemplateElement) {
+      source = source.content;
+    }
+
+    if (source instanceof DocumentFragment) {
+      let children: HTMLCollection = source.children;
+
+      if (children.length === 1) {
+        source = children[0] as HTMLElement;
+      } else {
+        source = children;
+      }
+    }
+
+    if (source instanceof HTMLElement) {
+      this.parentViewElement = new ViewElement(source, [(element) => new ViewElement(element)]);
+      source = source.children;
+    } else {
+      // no provided parent DOM element, use the fallback one
+      if (fallbackContainer) {
+        this.parentViewElement = new ViewElement(fallbackContainer, [
+          (element) => new ViewElement(element),
+        ]);
+      } else {
+        throw new ReferenceError('fallbackContainer not provided when needed');
+      }
+    }
+
+    const numDomElement: number = (source as any).length;
+    if (
+      shouldLazyInitialize === true ||
+      (shouldLazyInitialize === undefined &&
+        (numDomElement ===
+          undefined /* when we do not know the length, we conservatively choose to lazily initialize */ ||
+          numDomElement >= ViewElementProvider.LAZY_INITIALIZATION_THRESHOLD))
+    ) {
+      const viewElementCollection = new LazyCollectionProviderWithMaterializationCallback<
+        ViewElement
+      >(
+        (function* () {
+          for (const element of source) {
+            yield new ViewElement(element as HTMLElement);
+          }
+        })()
+      );
+      /**
+       * Instead of inserting a just-materialized view element at specified index, we append it at the end. This is because (1) materialization happen in order so next materialized element should be added as last view element child of `parentViewElement` (2) mutation of children `ViewElement` might happen during materialization so inserting at original place might create unexpected effect. For example, if a child ViewElement was removed, inserting at original place will create an empty slot.
+       */
+      viewElementCollection.materializationCallback = (_, child) =>
+        this.parentViewElement.insertChild__(child);
+      this.getChildViewElementsInternal = () => {
+        if (viewElementCollection.materialized) {
+          // all ViewElement has been registered under `parentViewElement`
+          this.getChildViewElementsInternal = () => this.parentViewElement.children_;
+          return this.parentViewElement.children_;
+        } else {
+          return viewElementCollection;
+        }
+      };
+    } else {
+      // create all ViewElement immediately
+      this.parentViewElement.patchChildViewElementsWithDOMElements__(source);
+      this.getChildViewElementsInternal = () => this.parentViewElement.children_;
+    }
+  }
+}
