0ec06b4782f3a199a127c7d6869fc7262273b37e
pengzhengyi <pengzhengyipengzhengyi@gmail.com>
2021-04-21 02:40:59 +0800

    update eslint settings to enable linting for typescript files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
   plugins: ['jest', '@typescript-eslint'],
   extends: [
     'eslint:recommended',
