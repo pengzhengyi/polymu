f48c4b28236a6285e9fc42ae8c18210168db6838
pengzhengyi <pengzhengyipengzhengyi@gmail.com>
2021-04-25 18:45:20 +0800

    refactor quickSort method to add a helper

--- src/utils/ArrayHelper.ts
+++ src/utils/ArrayHelper.ts
@@ -38,30 +38,40 @@ export function quickSort<T>(
         swap(array, start, start + 1);
       }
       return;
-    default: {
-      let pivotIndex: number = getRandomInt(start, difference);
-      const pivotElement: T = array[pivotIndex];
-      if (pivotIndex !== start) {
-        array[pivotIndex] = array[start];
-        pivotIndex = start;
-      }
+    default:
+      quickSortForMoreThanTwoElements(array, comparator, start, end);
+      return;
+  }
+}
 
-      // quick sort
-      for (let i = start + 1; i < end; i++) {
-        const element: T = array[i];
-        if (comparator(element, pivotElement) < 0) {
-          // current element is smaller than the pivot element in comparison ordering
-          array[pivotIndex++] = element;
-          if (pivotIndex !== i) {
-            // there exists elements greater than the pivot element at this point
-            array[i] = array[pivotIndex];
-          }
-        }
-      }
+function quickSortForMoreThanTwoElements<T>(
+  array: Array<T>,
+  comparator: (element1: T, element2: T) => number,
+  start = 0,
+  end: number = array.length
+) {
+  const difference: number = end - start;
+  let pivotIndex: number = getRandomInt(start, difference);
+  const pivotElement: T = array[pivotIndex];
+  if (pivotIndex !== start) {
+    array[pivotIndex] = array[start];
+    pivotIndex = start;
+  }
 
-      array[pivotIndex] = pivotElement;
-      quickSort(array, comparator, start, pivotIndex);
-      quickSort(array, comparator, pivotIndex + 1, end);
+  // quick sort
+  for (let i = start + 1; i < end; i++) {
+    const element: T = array[i];
+    if (comparator(element, pivotElement) < 0) {
+      // current element is smaller than the pivot element in comparison ordering
+      array[pivotIndex++] = element;
+      if (pivotIndex !== i) {
+        // there exists elements greater than the pivot element at this point
+        array[i] = array[pivotIndex];
+      }
     }
   }
+
+  array[pivotIndex] = pivotElement;
+  quickSort(array, comparator, start, pivotIndex);
+  quickSort(array, comparator, pivotIndex + 1, end);
 }
